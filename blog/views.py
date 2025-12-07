830e62ba53816d39c87cb4d0b1b1e96e90474a9b
s1f10230236 <136302397+s1f10230236@users.noreply.github.com>
2025-12-08 00:05:01 +0900

    10-1

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,10 @@ def update(request, article_id):
 	return HttpResponse("article_id: {}".format(article_id))
 
 def hello(request):
-    return render(request, 'blog/hello.html')
+	data={
+		"name":"Alice",
+		"weather_detail":["Temperature:23℃","Humidity:40%","Wind:5m/s"],
+		"isGreatFortune":True,
+		"fortune":"Great Fortune!"
+	}
+	return render(request, 'blog/hello.html',data)
