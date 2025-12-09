0c19611aa055dd1036cd4f9bd9c185b93b8fc072
s1f10230236 <136302397+s1f10230236@users.noreply.github.com>
2025-12-09 17:44:32 +0900

    11-1

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
 	path('', views.index, name='index'),
     path('<int:article_id>/update', views.update, name='update'),
     path('hello', views.hello, name='hello')
+    path('redirect',views.redirect,name='redirect_test')
+    path('<int:article_id>/',views.detail,name='detail')
+    path('<int:article_id>/delete',views.delete,name='delete')
+    path('<int:article_id>/update',views.detail,name='update')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.http import Http404
+from django.utils import timezone
+import random
+from blog.models import Article
 
 def index(request):
-	return render (request, 'blog/index.html')
+	if request.method=='POST':
+		article=Article(title=request.POST['title'],body=request.POST['text'])
+		article.save()
+		return redirect(detail,article.id)
+	context = {
+        "articles":Article.objects.all()
+    }
+	return render(request, 'blog/index.html', context)
+
+def redirect_test(redirect):
+	return redirect(hello)
+
+def detail(request,article_id):
+	try:
+		article=Article.objects.get(pk=article_id)
+	except Article.DoesNotExist:
+		raise Http404('Article does not exist')
+	context={
+		'article':article
+	}
+	return render(request,'blog/detail.html',context)
 
 def update(request, article_id):
-	return HttpResponse("article_id: {}".format(article_id))
+	try:
+		article=Article.objects.get(pk=article_id)
+	except Article.DoesNotExist:
+		raise Http404('Article does not exist')
+	if request.method=='POST':
+		article.title=request.POST['title']
+		article.body=request.POST['text']
+		article.save()
+		return redirect(detail,article_id)
+	context={
+		'article':article
+	}
+	return render(request,"blog/edit.html",context)
+
+def delete(request,article_id):
+	try:
+		article=Article.objects.get(pk=article_id)
+	except Article.DoesNotExist:
+		raise Http404('Article does not exist')
+	article.delete()
+	return redirect(index)
 
 def hello(request):
 	data={
